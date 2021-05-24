85ec70d52383b8d3b4a592124deb7047d1a35a68
kain <zhangtqx@vip.qq.com>
2021-05-25 00:18:13 +0800

    update console

--- src/app/layout/nav-items.ts
+++ src/app/layout/nav-items.ts
@@ -2,7 +2,7 @@ import { LayoutNav } from '@layout/types';
 
 export const navItems: LayoutNav[] = [
   { name: 'Dashboard', icon: 'dashboard', router: 'dashboard' },
-  { name: 'Workbench', icon: 'desktop', router: 'workbench' },
+  { name: 'Workbench', icon: 'laptop', router: 'workbench' },
   { name: 'Projects', icon: 'project', router: 'projects' },
   { name: 'Console', icon: 'code', router: 'console' }
 ];

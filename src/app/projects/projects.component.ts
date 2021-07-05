ea4a237b9abfae62eeced29af03680cdc6365026
kain <zhangtqx@vip.qq.com>
2021-07-05 22:06:19 +0800

    update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,19 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html'
+  selector: "app-projects",
+  templateUrl: "./projects.component.html"
 })
 export class ProjectsComponent implements OnInit {
   lists: any[] = [
     {
-      name: '解决方案 A'
-    },
-    {
-      name: '解决方案 B'
-    },
-    {
-      name: '解决方案 C'
+      name: "解决方案 Test"
     }
   ];
 

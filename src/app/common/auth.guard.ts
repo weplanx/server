512a1502ec1fb789c312395377ac674eae5dbc55
kain <zhangtqx@vip.qq.com>
2021-07-07 20:47:11 +0800

    update

--- src/app/common/auth.guard.ts
+++ src/app/common/auth.guard.ts
@@ -1,22 +1,24 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthGuard implements CanActivate {
   constructor(
+    private http: HttpClient,
     private router: Router
   ) {
   }
 
-  canActivate(): Observable<boolean> {
-    return of(false).pipe(
-      map(v => {
-        if (!v) {
-          this.router.navigateByUrl('/login');
+  canActivate(): Observable<any> {
+    return this.http.get("/api/verify").pipe(
+      map((res: any) => {
+        if (res.error) {
+          this.router.navigateByUrl("/login");
         }
-        return v;
+        return !res.error;
       })
     );
   }
